98f740217713793804d6aed235c1cfbc7f37cf13
JSonWulff <julianwulff@gmail.com>
2019-03-15 14:24:21 +0100

    Added squadron column method

--- SU19-Exercises/Galaga-Exercise-2/Squadrons/Column.cs
+++ SU19-Exercises/Galaga-Exercise-2/Squadrons/Column.cs
@@ -1,14 +1,38 @@
 using System.Collections.Generic;
 using DIKUArcade.Entities;
 using DIKUArcade.Graphics;
+using DIKUArcade.Math;
 using Galaga_Exercise_2.GalagaEnities.Enemy;
 
 namespace Galaga_Exercise_2.Squadrons {
     public class Column : ISquadron {
+        private Game game;
         public EntityContainer<Enemy> Enemies { get; }
         public int MaxEnemies { get; }
+
+        public Column(Game game) {
+            this.game = game;
+            MaxEnemies = 3;
+            Enemies = new EntityContainer<Enemy>();
+        }
+
+
         public void CreateEnemies(List<Image> enemyStrides) {
-            throw new System.NotImplementedException();
+            for (var i = 0; i < MaxEnemies; i++) {
+                Enemies.AddDynamicEntity(new Enemy(game,
+                    new DynamicShape(
+                        new Vec2F(
+                            0.1f, 
+                            0.90f - 0.1f * i), 
+                        new Vec2F(0.1f, 0.1f)),
+                        new ImageStride(80, enemyStrides)));
+            }
+        }
+
+        public void RenderEnemies() {
+            foreach (Enemy enemy in Enemies) {
+                enemy.RenderEntity();
+            }
         }
     }
 }
