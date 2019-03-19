81669aed13ba81661c8f00592c5844a2b59e27ef
JSonWulff <julianwulff@gmail.com>
2019-03-19 12:09:26 +0100

    Added GalagaBus class

---
+++ SU19-Exercises/Galaga-Exercise-3/GalagaBus.cs
@@ -0,0 +1,10 @@
+using DIKUArcade.EventBus;
+
+namespace Galaga_Exercise_3 {
+    public class GalagaBus {
+        private static GameEventBus<object> eventBus;
+        public static GameEventBus<object> GetBus() {
+            return GalagaBus.eventBus ?? (GalagaBus.eventBus = new GameEventBus<object>());
+        }
+    }
+}
