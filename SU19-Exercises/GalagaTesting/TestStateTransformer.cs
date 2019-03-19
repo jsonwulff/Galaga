d9d7d0a4203a67391e2c338fff2b22b908fde472
JSonWulff <julianwulff@gmail.com>
2019-03-19 15:34:02 +0100

    Modified the layout of the test files

--- SU19-Exercises/GalagaTesting/TestStateTransformer.cs
+++ SU19-Exercises/GalagaTesting/TestStateTransformer.cs
@@ -1,5 +1,14 @@
+using System;
+using NUnit.Framework;
+
 namespace Galaga_Testing {
     public class TestStateTransformer {
-        
+        [TestFixture]
+        public class Tests {
+            [Test]
+            public void Test1() {
+                Assert.True(true);
+            }
+        }
     }
 }
