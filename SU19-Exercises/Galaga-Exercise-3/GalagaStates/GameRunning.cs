d80fcd749bb1b049ea52f0b7ed2466057f6ffd74
Mads Christensen <mads.bchrist@gmail.com>
2019-03-20 12:10:21 +0100

    partly implemented GameRunning and GamePaused, implemented StateMachine.SwitchState

---
+++ SU19-Exercises/Galaga-Exercise-3/GalagaStates/GameRunning.cs
@@ -0,0 +1,30 @@
+using DIKUArcade.State;
+
+namespace Galaga_Exercise_3.GalagaStates {
+    public class GameRunning : IGameState {
+        private static GameRunning instance = null;
+        public static GameRunning GetInstance() {
+            return GameRunning.instance ?? (GameRunning.instance = new GameRunning());
+        }
+        
+        public void GameLoop() {
+            throw new System.NotImplementedException();
+        }
+
+        public void InitializeGameState() {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateGameLogic() {
+            throw new System.NotImplementedException();
+        }
+
+        public void RenderState() {
+            throw new System.NotImplementedException();
+        }
+
+        public void HandleKeyEvent(string keyValue, string keyAction) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- SU19-Exercises/Galaga-Exercise-3/GalagaStates/StateMachine.cs
+++ SU19-Exercises/Galaga-Exercise-3/GalagaStates/StateMachine.cs
@@ -4,19 +4,42 @@ using DIKUArcade.State;
 namespace Galaga_Exercise_3.GalagaStates {
     public class StateMachine : IGameEventProcessor<object> {
         public IGameState ActiveState { get; private set; }
-        
+
         public StateMachine() {
             GalagaBus.GetBus().Subscribe(GameEventType.GameStateEvent, this);
             GalagaBus.GetBus().Subscribe(GameEventType.InputEvent, this);
-            
+
             ActiveState = MainMenu.GetInstance();
         }
+
         private void SwitchState(GameStateType stateType) {
-            switch (stateType) { }
+            switch (stateType) {
+            case (GameStateType.GameRunning):
+                ActiveState = GameRunning.GetInstance();
+                break;
+            case (GameStateType.GamePaused):
+                ActiveState = GamePaused.GetInstance();
+                break;
+            case (GameStateType.MainMenu):
+                ActiveState = MainMenu.GetInstance();
+                break;
+            }
         }
 
         public void ProcessEvent(GameEventType eventType, GameEvent<object> gameEvent) {
-            throw new System.NotImplementedException();
+            if (eventType == GameEventType.GameStateEvent) {
+                switch (gameEvent.Message) {
+                case "GAME_RUNNING":
+                    SwitchState(GameStateType.GameRunning);
+                    break;
+                case "GAME_PAUSED":
+                    SwitchState(GameStateType.GamePaused);
+                    break;
+                case "MAIN_MENU":
+                    SwitchState(GameStateType.MainMenu);
+                    break;
+                }
+            }
         }
     }
 }
