345f3e6a5ca793ad2299fb08be777308a11bd9bd
JSonWulff <julianwulff@gmail.com>
2019-03-20 12:24:08 +0100

    Updated StateMachineTesting

--- SU19-Exercises/GalagaTesting/StateMachineTesting.cs
+++ SU19-Exercises/GalagaTesting/StateMachineTesting.cs
@@ -1,18 +1,25 @@
+using System.Collections.Generic;
 using DIKUArcade.EventBus;
 using Galaga_Exercise_3;
 using Galaga_Exercise_3.GalagaStates;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 namespace Galaga_Testing {
     [TestFixture]
     public class StateMachineTesting {
         private StateMachine stateMachine;
+        
         [SetUp]
         public void InitiateStateMachine() {
             DIKUArcade.Window.CreateOpenGLContext();
             // Here you should:
             // (1) Initialize a GalagaBus with proper GameEventTypes
+            GalagaBus.GetBus().InitializeEventBus(new List<GameEventType>( {
+                    GameEventType.GameStateEvent
+                });
             // (2) Instantiate the StateMachine
+            stateMachine = new StateMachine();
             // (3) Subscribe the GalagaBus to proper GameEventTypes
             //     and GameEventProcessors
 
