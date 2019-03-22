d86fd19e08cbb72cc51dceafc3621b4154e1f49a
Mads Christensen <mads.bchrist@gmail.com>
2019-03-22 10:51:34 +0100

    Implemented MainMenu.cs

--- SU19-Exercises/Galaga-Exercise-3/GalagaStates/MainMenu.cs
+++ SU19-Exercises/Galaga-Exercise-3/GalagaStates/MainMenu.cs
@@ -1,9 +1,13 @@
+using System.Drawing;
 using System.IO;
+using System.Linq;
+using DIKUArcade;
 using DIKUArcade.Entities;
 using DIKUArcade.EventBus;
 using DIKUArcade.Graphics;
 using DIKUArcade.Math;
 using DIKUArcade.State;
+using Image = DIKUArcade.Graphics.Image;
 
 namespace Galaga_Exercise_3.GalagaStates {
     public class MainMenu : IGameState {
@@ -11,11 +15,25 @@ namespace Galaga_Exercise_3.GalagaStates {
 
         private Entity backGroundImage;
         private Text[] menuButtons;
+        private Text newGame;
+        private Text quit;
         private int activeMenuButton;
-        private int maxMenuButtons;
+        private int maxMenuButtons = 2;
+        private Vec3I activeColor;
+        private Vec3I inactiveColor;
 
         public MainMenu() {
+            activeMenuButton = 0;
             backGroundImage = new Entity(new StationaryShape(new Vec2F(0,0), new Vec2F(1,1) ), new Image(Path.Combine( "Assets",  "Images", "TitleImage.png")));
+            menuButtons = new Text[maxMenuButtons];
+            newGame = new Text("New Game", new Vec2F(1, 1), new Vec2F(1, 1));
+            quit = new Text("Quit", new Vec2F(1, 1), new Vec2F(1, 1));
+            menuButtons.Append(newGame);
+            menuButtons.Append(quit);
+            
+            activeColor = new Vec3I(255,255,255);
+            inactiveColor = new Vec3I(190,190,190);
+            
         }
         
         public static MainMenu GetInstance() {
@@ -31,20 +49,68 @@ namespace Galaga_Exercise_3.GalagaStates {
         }
 
         public void UpdateGameLogic() {
-            throw new System.NotImplementedException();
+            foreach (var button in menuButtons) {
+                button.RenderText();
+            }
         }
 
         public void RenderState() {
             backGroundImage.RenderEntity();
         }
 
+        public void HandleButtons() {
+            foreach (var button in menuButtons) {
+                button.SetColor(inactiveColor);                
+            }
+            menuButtons[activeMenuButton % maxMenuButtons].SetColor(activeColor);
+        }
+
+        public void ActivateButton() {
+            switch (activeMenuButton) {
+            case 0:
+                GalagaBus.GetBus().RegisterEvent(
+                    GameEventFactory<object>.CreateGameEventForAllProcessors(
+                        GameEventType.WindowEvent, this, "CHANGE_STATE", "NEW_GAME", ""));
+                break;
+            case 1:
+                GalagaBus.GetBus().RegisterEvent(
+                    GameEventFactory<object>.CreateGameEventForAllProcessors(
+                        GameEventType.WindowEvent, this, "CLOSE_WINDOW", "", ""));
+                break;
+            }
+        }
+        
+        public void KeyPress(string key) {
+            switch (key) {
+            case "KEY_UP":
+                activeMenuButton -= 1;
+                HandleButtons();
+                break;
+            case "KEY_DOWN":
+                activeMenuButton += 1;
+                break;
+            case "KEY_ENTER":
+                ActivateButton();
+                break;
+            }
+        }
+        
+        /// <summary>
+        /// KeyRelease handles logic when a key sent by ProcessEvent is released.
+        /// </summary>
+        /// <param name="key"></param>
+        public void KeyRelease(string key) {
+            switch (key) {
+
+            }
+        }
+        
         public void HandleKeyEvent(string keyValue, string keyAction) {
             switch (keyAction) {
                 case "KEY_PRESS":
-                    
+                    KeyPress(keyValue);
                     break;
                 case "KEY_RELEASE":
-                    
                     break;
             }
         }
